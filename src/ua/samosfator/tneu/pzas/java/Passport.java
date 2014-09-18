41ee1248b680f012d4663df658e9ae5d2e3ab776
samosfator <golubev.vld@gmail.com>
2014-09-18 15:57:52 +0300

    Groups passport project

---
+++ src/ua/samosfator/tneu/pzas/java/Passport.java
@@ -0,0 +1,30 @@
+package ua.samosfator.tneu.pzas.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Passport {
+    static Scanner in = new Scanner(System.in);
+    static int i;
+
+    public static void main(String[] args) {
+        List<String> passport = new ArrayList<>();
+        passport.add("Назва групи: ПЗАС-12");
+        passport.add("Дата народження: 01.09.2014");
+        passport.add("Місто: Файне місто Тернопіль");
+        passport.add("Батько: Дивак Микола Петрович");
+        passport.add("Мати: Гончар Людмила Іванівна");
+        passport.add("Національність: Програмісти");
+        passport.add("Місце прописки: Тернопільський національний економічний університет");
+
+        System.out.println("Паспорт групи:");
+
+        while (i < 7) {
+            if (in.nextLine().contains(" ")) {
+                System.out.println(passport.get(i));
+                i++;
+            }
+        }
+    }
+}
