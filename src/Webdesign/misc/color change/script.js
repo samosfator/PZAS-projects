c8fecd6f42f882f6482bba1d2b422fe4cfa49131
samosfator <golubev.vld@gmail.com>
2014-10-09 21:52:52 +0300

    Just4Fun color changing in JS

---
+++ src/Webdesign/misc/color change/script.js
@@ -0,0 +1,29 @@
+var body = document.getElementsByTagName("body")[0];
+var r = 150,
+	g = 1,
+	b = 1;
+var increaseR = true, increaseG = true, increaseB = true;
+
+setInterval(function() {
+	if (r < 255 && increaseR) {
+		r++;
+		if (b > 2) b--;
+	} else {
+		increaseR = false;
+		if (g < 255 && increaseG) {
+			g++;
+			r--;
+		} else {
+			increaseG = false;
+			if (b < 255 && increaseB) {
+				b++;
+				g--;
+			} else {
+				//increaseB = false;
+				increaseR = true;
+			}
+		}
+	}
+
+	body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+}, 1);
