db9e7e4d535b25fc40b9f460ad70e875cc82c48e
samosfator <golubev.vld@gmail.com>
2014-10-10 22:07:38 +0300

    Power 2 to 100 in C++ using double variable

--- src/Webdesign/cpit/js/script.js
+++ src/Webdesign/cpit/js/script.js
@@ -1,1 +1,5 @@
 var currentEducationalWeek = moment().week() - moment("20140901", "YYYYMMDD").week() + 1;
+
+$(document).ready(function() {
+	console.log("Зараз " + currentEducationalWeek + " тиждень навчання");
+});
