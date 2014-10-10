5fcf5dabcf85aad39586965bcd94c31d5e8d7826
samosfator <golubev.vld@gmail.com>
2014-10-10 22:07:50 +0300

    Revert "Power 2 to 100 in C++ using double variable"
    This reverts commit db9e7e4d535b25fc40b9f460ad70e875cc82c48e.

--- src/Webdesign/cpit/js/script.js
+++ src/Webdesign/cpit/js/script.js
@@ -1,5 +1,1 @@
 var currentEducationalWeek = moment().week() - moment("20140901", "YYYYMMDD").week() + 1;
-
-$(document).ready(function() {
-	console.log("Зараз " + currentEducationalWeek + " тиждень навчання");
-});
